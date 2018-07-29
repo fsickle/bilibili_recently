2e6528185acd2687a38b8bff60bcdb33c12d2f0e
wangker <wanghyscu@163.com>
2018-07-29 11:38:23 +0800

    'robotstext_obey'

--- bilibili_recently/settings.py
+++ bilibili_recently/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'bilibili_recently.spiders'
 #USER_AGENT = 'bilibili_recently (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
