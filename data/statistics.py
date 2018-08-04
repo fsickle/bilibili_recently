da86e8f3b4e51ab6e591ebaba3960b1813b7d768
fsickle <wanghyscu@163.com>
2018-08-04 11:30:06 +0800

    statistics

--- data/statistics.py
+++ data/statistics.py
@@ -1,27 +1,35 @@
 import pymongo
 import pygal
 
-client = pymongo.MongoClient('localhost')
-db = client.bilibili
-data = db.recently_submission
+class Result():
 
-list = ['王者荣耀', '300英雄', '绝地求生', 'CS：GO', '穿越火线', '英雄联盟', '刺激战场', 'QQ飞车', '第五人格', '守望先锋',
-        '星际争霸', '彩虹6号', 'DOTA2', '风暴英雄', '炉石传说', '决战平安京', '皇室战争', '崩坏']
+    client = pymongo.MongoClient('localhost')
+    db = client.bilibili
+    data = db.recently_submission
 
-count = []
-for i in list:
-    a = data.find({'logs':{'$regex': i}}).count()
-    count.append(a)
+    list = ['王者荣耀', '300英雄', '绝地求生', 'CS：GO', '穿越火线', '英雄联盟', '刺激战场', 'QQ飞车', '第五人格', '守望先锋',
+            '星际争霸', '彩虹6号', 'DOTA2', '风暴英雄', '炉石传说', '决战平安京', '皇室战争', '崩坏']
 
-my_config = pygal.Config()
-# x轴的文字旋转45度
-my_config.x_label_rotation = -45
-result = pygal.Bar(my_config)
+    def picture(self):
+        count = []
+        for i in self.list:
+            a = self.data.find({'logs':{'$regex': i}}).count()
+            count.append(a)
 
-result.title = "Results of Games Upload Counts."
-result.x_labels = list
-result.x_title = 'Games'
-result.y_title = 'Counts'
+        my_config = pygal.Config()
+        # x轴的文字旋转45度
+        my_config.x_label_rotation = -45
+        result = pygal.Bar(my_config)
 
-result.add('Bilibili', count)
-result.render_to_file('result_visual.svg')
+        result.title = "Results of Games Upload Counts."
+        result.x_labels = list
+        result.x_title = 'Games'
+        result.y_title = 'Counts'
+
+        result.add('Bilibili', count)
+        result.render_to_file('result_visual.svg')
+
+
+if __name__ == '__main__':
+    result = Result()
+    result.picture()
